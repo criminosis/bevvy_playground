c37ffc5d66f8628006c98d1863e6466f0c72ed4a
Allan Clements <criminosis@gmail.com>
2020-09-06 08:45:31 -0500

    First startup system

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,34 @@ use bevy::prelude::*;
 fn main() {
     //Being able to turn a method into a system is done via "trait extension"
     //http://xion.io/post/code/rust-extension-traits.html
-    App::build().add_system(hello_world.system()).run();
+    App::build()
+        //Startup System is only ran once - at startup
+        .add_startup_system(add_people.system())
+        //normal systems run continuously once looped?
+        .add_system(greet_people.system())
+        .add_system(hello_world.system())
+        .run();
 }
 
 fn hello_world() {
     println!("hello world!");
+}
+
+fn greet_people(_person: &Person, name: &Name) {
+    println!("hello {}!", name.0);
+}
+
+//A person component
+struct Person;
+
+//We could put name under person, but what if we want to put names to other things e.g. a pet
+//So ECS practice is to separate Name to its own component so it can be composed into mutiple entities
+struct Name(String);
+
+//Initializing a set of People components to our App
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn((Person, Name("Elaina Proctor".to_string())))
+        .spawn((Person, Name("Renzo Hume".to_string())))
+        .spawn((Person, Name("Zayna Nieves".to_string())));
 }
