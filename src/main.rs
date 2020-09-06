6d1d3a0a1a2a1fc56efce49bd8404824081680e0
Allan Clements <criminosis@gmail.com>
2020-09-06 08:36:33 -0500

    Added note about trait extension

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 
 fn main() {
+    //Being able to turn a method into a system is done via "trait extension"
+    //http://xion.io/post/code/rust-extension-traits.html
     App::build().add_system(hello_world.system()).run();
 }
 
